c8733e5388f861ee7dc2e3b2798e7314cdbcf6d9
cheroshena <fwpcheroshena@gmail.com>
2023-03-29 23:28:22 +0530

    cheroshena-patch-1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,82 @@
 import React from 'react';
 import { NavLink,Link } from 'react-router-dom';
+import {BsSearch} from "react-icons/bs";
 const Header = () => {
   return (
     <>
       <header className="header-top-strip py-3" >
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <p className="text-white mb-0">Free Shipping Over $100 & Free Returns</p><Link>
+              
+              </Link></div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:{" "}
+                <a className="text-white" href="(011) 892 6000">(011) 892 6000</a>
+                </p>
+                </div>
+
+          </div>
+
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+            <h2>
+              <Link className="text-white">E-Doctor</Link>
+            </h2>
+            </div>
+            
+            <div className="col-5">
+              <div class="input-group">
+             <input type="text" 
+             className="form-control" 
+             placeholder="Search Product Here.." 
+             aria-label="Search Product Here.." 
+             aria-describedby="basic-addon2"
+             />
+            <span className="input-group-text p-3" id="basic-addon2">
+              <BsSearch className="fs-6" />
+              </span>
+              </div>
+              </div>
+
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link>
+                  <img src="/images/compare.svg" alt=""/>
+                  <p></p>
+
+                </Link>
+                </div>
+                <div>
+                  <Link>
+                  <img src="" alt=""/>
+                  <p></p>
+                
+                </Link>
+                </div>
+                <div>
+                  <Link>
+                  <img src="" alt=""/>
+                  <p></p>
+                
+                </Link>
+                </div>
+                <div>
+                  <Link>
+                  <img src="" alt=""/>
+                  <p></p>
+                
+                </Link>
+                </div>
+              </div>
+            </div>
 
           </div>
 
