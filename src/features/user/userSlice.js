afbaed99380b867d44756ca86eded1ddf24074af
cheroshena <fwpcheroshena@gmail.com>
2023-04-18 02:25:22 +0530

    login & register

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,84 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { authService } from "./userService";
+import { toast } from "react-toastify";
+
+//Register user
+export const registerUser = createAsyncThunk("auth/register", async (userData, thunkAPI) => {
+    try {
+        return await authService.register(userData)
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+})
+
+//Login user
+export const loginUser = createAsyncThunk("auth/login", async (userData, thunkAPI) => {
+    try {
+        return await authService.login(userData)
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error)
+    }
+})
+const initialState = {
+    user: "",
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ""
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(registerUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isError = false;
+                state.isSuccess = true;
+                state.createdUser = action.payload;
+                if (state.isSuccess === true) {
+                    toast.info("User Created Successfully!");
+                }
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.isSuccess = false;
+                state.message = action.error;
+                if (state.isError === true) {
+                    toast.error(action.error);
+                }
+            })
+            .addCase(loginUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(loginUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isError = false;
+                state.isSuccess = true;
+                state.user = action.payload;
+                
+                if (state.isSuccess === true) {
+                    localStorage.setItem("token",action.payload.token);
+                    toast.info("You are Logging Successfully !");
+                }
+            })
+            .addCase(loginUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.isSuccess = false;
+                state.message = action.error;
+                if (state.isError === true) {
+                    toast.error(action.error);
+                }
+            });
+    },
+});
+
+
+export default authSlice.reducer;
