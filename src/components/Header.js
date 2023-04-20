4b8686ef1aa504625cd47c43c7c4b8209013b360
cheroshena <fwpcheroshena@gmail.com>
2023-04-21 02:47:51 +0530

    header cart update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { BsSearch } from "react-icons/bs";
 import compare from "../images/compare.svg";
@@ -7,8 +7,20 @@ import user from "../images/user.svg";
 import cart from "../images/cart.svg";
 import menu from "../images/menu.svg";
 import Home from '../pages/Home';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const cartState = useSelector(state => state?.auth?.cartProducts)
+  const [total, setTotal] = useState(null)
+  useEffect(() => {
+    let sum = 0
+    for (let index = 0; index < cartState?.length; index++) {
+      
+      sum = sum + (Number(cartState[index]?.quantity) * Number(cartState[index]?.price))
+      setTotal(sum)
+    }
+  }, [cartState])
   return (
     <>
       <header className="header-top-strip py-3" >
@@ -79,8 +91,8 @@ const Header = () => {
                   <Link to="/cart" className="d-flex align-items-center gap-10 text-white">
                     <img src={cart} alt="cart" />
                     <div className="d-flex flex-column gap-10">
-                      <span className="badge bg-white text-dark">0</span>
-                      <p className="mb-0">$500</p>
+                      <span className="badge bg-white text-dark">{cartState?.length ? cartState?.length :0}</span>
+                      <p className="mb-0">Rs. {total ? total : 0}</p>
 
                     </div>
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -10,6 +10,8 @@ const Cart = () => {
 
     const dispatch = useDispatch();
     const [productUpdateDetail, setProductUpdateDetail] = useState(null)
+    const [totalAmount,setTotalAmount]=useState(null)
+    console.log(totalAmount);
 
     const userCartState = useSelector(state => state?.auth?.cartProducts)
     useEffect(() => {
@@ -32,9 +34,14 @@ const Cart = () => {
         }, 200)
     }
 
-    const updateACartProduct = (productUpdateDetail) => {
+    useEffect(() => {
+        let sum = 0;
+        for (let index = 0; index < userCartState?.length; index++) {
+            sum=sum+(Number(userCartState[index]?.quantity)* userCartState[index]?.price )
+            setTotalAmount(sum)
 
-    }
+        }
+    }, [userCartState])
     return (
         <>
             <Meta title={"Cart"} />
@@ -98,13 +105,16 @@ const Cart = () => {
                         <Link to="/product" className="button">
                             Continue To Shopping
                         </Link>
-                        <div className="d-flex flex-column align-items-end">
-                            <h4>SubTotal: $ 1000</h4>
+                        {
+                            (totalAmount!==null || totalAmount !==0) &&
+                            <div className="d-flex flex-column align-items-end">
+                            <h4>SubTotal: Rs. {totalAmount}.00</h4>
                             <p>Taxes and shipping calculated at checkout</p>
                             <Link to="/checkout" className="button">
                                 Checkout
                             </Link>
                         </div>
+                        }
                     </div>
                 </div>
 
